1cb85448bffa6c1a0c55bd09258b11c329933943
kahlflekzy <felixsigit@gmail.com>
2023-10-17 13:56:13 +0000

    Updates to code involving algorithm to solving the problem

--- nodes/node.py
+++ nodes/node.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python
 
+import cv2
 import rospy
+import tf2_ros
+import threading
+from cv_bridge import CvBridge
 from geometry_msgs.msg import PoseStamped
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CameraInfo
 from vector_map_msgs.msg import SignalArray, VectorArray, PointArray
+from visualization_msgs.msg import Marker
 from math import sqrt
+import numpy as np
+
+np.set_printoptions(precision=3)
+
 
 class Node:
     """"""
     def __init__(self):
         """"""
-        self.loop_rate = 1 # check for traffic lights and draw ROIs in Hz
+        self.loop_rate = 5 # check for traffic lights and draw ROIs in Hz
 
         self.min_distance = 0
         self.max_distance = 0
@@ -22,12 +31,41 @@ class Node:
         self.vectors = {}
         self.points = {}
         self.filtered_points = []
+        self.marker = Marker()
+        self.marker.header.frame_id = "map"
+        self.marker.type = Marker.SPHERE
+        self.marker.pose.position.z = 5;
+        self.marker.pose.orientation.x = 0.0;
+        self.marker.pose.orientation.y = 0.0;
+        self.marker.pose.orientation.z = 0.0;
+        self.marker.pose.orientation.w = 1.0;
+        res = 4
+        self.marker.scale.x = res;
+        self.marker.scale.y = res;
+        self.marker.scale.z = res;
+
+        self.marker.color.a = 1.0; 
+        self.marker.color.r = 1.0;
+        self.marker.color.g = 1.0;
+        self.marker.color.b = 1.0;
+        self.lights = 0
+
+        self.bridge = CvBridge()
 
         self.superimposed_img_pub = None
 
         self.current_image = None
+        self.camera_params = None
 
         self.set_parameters()
+
+        self.transform = None
+        self.tf_buffer = tf2_ros.Buffer()
+        listener = tf2_ros.TransformListener(self.tf_buffer)
+
+        
+        thread = threading.Thread(target=self.tf_listener)
+        thread.start()
 
         self.init_subscribers()
         self.init_publishers()
@@ -51,7 +89,9 @@ class Node:
 
     def init_publishers(self):
         """"""
-        self.superimposed_img_pub = rospy.Publisher("/camera_fl/tlr_superimpose_image", Image, queue_size=5)
+        self.superimposed_img_pub = rospy.Publisher("/camera_fl/tlr_superimpose_image", Image, queue_size=10)
+        self.camera_pub = rospy.Publisher("/camera_fl/camera_info", CameraInfo, queue_size=5)
+        self.marker_pub = rospy.Publisher("/node/markers", Marker, queue_size=10)
         rospy.sleep(2)
     
     def init_subscribers(self):
@@ -63,6 +103,7 @@ class Node:
         self.vectors_subscriber = rospy.Subscriber("/vector_map_info/vector", VectorArray, self.get_vectors)
         self.points_subscriber = rospy.Subscriber("/vector_map_info/point", PointArray, self.get_points)
         rospy.Subscriber("/camera_fl/decompressed/image_raw", Image, self.get_current_image)
+        self.camera_subscriber = rospy.Subscriber("/camera_fl/camera_info", CameraInfo, self.get_camera_params)
         rospy.loginfo("Finished initializing subscribers.")
     
     def get_car_pose(self, data):
@@ -89,7 +130,7 @@ class Node:
     def get_signals(self, signal):
         """"""
         data = signal.data
-        self.signals = {i.vid: i.type for i in data}
+        self.signals = {i.vid: i for i in data}
         rospy.loginfo("Got %d signals"%len(self.signals))
         self.signals_subscriber.unregister()
 
@@ -111,7 +152,7 @@ class Node:
         """
         Leave only yellow signals
         """
-        yellows = [vid for vid, type_ in self.signals.items() if type_ == 2]
+        yellows = [vid for vid, signal in self.signals.items() if signal.type == 2]
         pids = [self.vectors[vid] for vid in yellows]
         self.filtered_points = [self.points[pid] for pid in pids]
         rospy.loginfo("Filtered out %d points"%len(self.filtered_points))
@@ -122,8 +163,8 @@ class Node:
         Nearest is defined as 2m <= distance between car and light <= 60m
         """
         distances = [(self.euclidean(point), point) for point in self.filtered_points]
-        distances = filter(lambda i: self.min_distance <= i[0] <= self.max_distance,
-                           distances)
+        distances = filter(lambda i: self.min_distance <= i[0] <= self.max_distance, distances)
+        # print(distances)
         return [i[1] for i in distances]
     
     def euclidean(self, point):
@@ -137,13 +178,98 @@ class Node:
         while not rospy.is_shutdown():
             lights = self.find_nearest_points()
             # continue from here draw roi for lights
-            rospy.loginfo("found %d lights"%len(lights))
+            # rospy.loginfo("found %d lights"%len(lights))
+            self.marker.header.stamp = rospy.Time.now()
+            self.marker.action = self.marker.DELETE
+            for i in range(self.lights):
+                self.marker.id = i
+                self.marker_pub.publish(self.marker)
+            self.marker.action = Marker.ADD;
+            self.lights = len(lights)
+            for index, point in enumerate(lights):
+                self.marker.id = index
+                output = self.transform_point(point)
+                self.marker.pose.position.x = point.ly;
+                self.marker.pose.position.y = point.bx;
+                coord = self.project(point=output)
+                if (0 <= coord[0] <= self.camera_params.width
+                    and
+                    0 <= coord[1] <= self.camera_params.height):
+                    # draw ROI
+                    print(coord)
+                self.marker_pub.publish(self.marker)
+
+            try:
+                cv_image = self.bridge.imgmsg_to_cv2(self.current_image, desired_encoding='passthrough')
+                start_point = (5, 5)
+                end_point = (420, 420)
+                color = (0, 255, 0)
+                thickness = 2
+                image = cv2.rectangle(cv_image, start_point, end_point, color, thickness)
+                image_message = self.bridge.cv2_to_imgmsg(image, encoding="passthrough")
+                self.superimposed_img_pub.publish(self.current_image)
+            except:
+                pass
             rate.sleep()
 
     def get_current_image(self, image):
         """"""
         self.current_image = image
-        self.superimposed_img_pub.publish(self.current_image)
+        # self.superimposed_img_pub.publish(self.current_image)
+    
+    def get_camera_params(self, data):
+        """"""
+        self.camera_params = data
+
+    def tf_listener(self):
+        """
+        """
+        rospy.loginfo("Listening for map to jackal0/base_link transforms.")
+        
+        rate = rospy.Rate(10)
+        while not rospy.is_shutdown():
+            try:
+                self.transform = self.tf_buffer.lookup_transform("map", "camera_fl", rospy.Time())
+            except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
+                pass
+            rate.sleep() 
+    
+    def transform_point(self, point):
+        """
+        Transforms a point in map frame to camera frame
+        """
+        output = [
+            point.ly - self.transform.transform.translation.x,
+            point.bx - self.transform.transform.translation.y,
+            point.h - self.transform.transform.translation.z,
+        ]
+        return output
+
+    def project(self, point):
+        """
+        Get pixel coordinates of point by transforming and then getting coordinates.
+        """
+        T0 = np.array([
+            [1, 0, 0, point[0]],
+            [0, 1, 0, point[1]],
+            [0, 0, 1, point[2]],
+            [0, 0, 0, 1]
+            ])
+        T1 = np.array([
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]
+            ])
+        T2 = np.array(self.camera_params.R).reshape(3, 3)
+        X = np.array(point).reshape(3, 1)
+        X_ = np.matmul(T2, X)
+        K = np.array(self.camera_params.K).reshape(3, 3)
+        xs = np.matmul(K, X_).flatten()
+        # print(xs)
+        return xs
+
+
 
 if __name__ == "__main__":
     rospy.init_node("roi_node")
--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
 # Traffic ROI
 ## Introduction
+Requires `numpy` to be installed
 ```
 cd /root/ROS/
 ```
@@ -25,13 +26,21 @@ sudo apt install ros-melodic-vector-map-msgs
 ```
 
 ```
+sudo apt install ros-melodic-vision-opencv
+```
+```
+sudo apt install ros-melodic-visualization-msgs
+```
+
+```
 catkin_create_pkg -D "A package which detects traffic lights from a map and projects them to images from a front facing camera, based on the car's current location." -l "BSD" --author "Felix Sihitshuwam" --maintainer "Felix Sihitshuwam" traffic_roi rospy roscpp std_msgs geometry_msgs vector_map_msgs
 ```
+Add `visualization_msgs` in find_package.
 ```
 cd .. && catkin build traffic_roi
 ```
 
-Copy Data from local to docker image, Ex.
+Hint to copy data from local machine to docker container, Ex.
 ```
 docker cp config.rviz uav_lander:/root/ROS/workspace/src/traffic_roi/data/
 ```
@@ -44,8 +53,23 @@ docker cp config.rviz uav_lander:/root/ROS/workspace/src/traffic_roi/data/
 ```
 roslaunch traffic_roi assignment.launch data:=data
 ```
+
+## Algorithm
+To solve this problem, the steps involved includes:
+1. Find all traffic lights. This can be achieved by 
+    > i. finding all signals, \
+    > ii. finding all vectors which relates signals to points \
+    > iii. finding all points and filtering points belonging to signals. This tells the location of the signals
+2. Find the traffic lights whose points are within a euclidean distance of `min_distance` and `max_distance`. Use `x & y` coordinates. 
+3. For each point (traffic light) close to the car's current location:
+    > I.   Transform `point` from the `map` frame to the `camera` frame \
+    > II.  Project `point` using the camera's intrinsic parameters to get the coordinates of the `point` in the image. \
+    > III. Using these coordinates as centre, draw a rectangle on the image indicating ROI.
+4. Loop while car is still moving or node is running.
+
 ## References
 1. [Docker cp SiteCore](https://support.sitecore.com/kb?id=kb_article_view&sysparm_article=KB0383441)
 2. [Docker cp Docker](https://docs.docker.com/engine/reference/commandline/cp/)
 3. [Rospy Param](http://wiki.ros.org/rospy/Overview/Parameter%20Server)
 4. [Adding local Repo to GitHub](https://docs.github.com/en/migrations/importing-source-code/using-the-command-line-to-import-source-code/adding-locally-hosted-code-to-github)
+5. [Visualization](http://wiki.ros.org/rviz/Tutorials/Markers%3A%20Basic%20Shapes)
